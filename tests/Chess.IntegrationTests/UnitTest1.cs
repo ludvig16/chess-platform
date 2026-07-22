414f61232f8deaac48cfbf640991de3d15dc4f38
lusj3822 <ludvigsjoberg160@gmail.com>
2026-07-22 17:38:15 +0200

    init project

---
+++ tests/Chess.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Chess.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Chess.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Chess.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
