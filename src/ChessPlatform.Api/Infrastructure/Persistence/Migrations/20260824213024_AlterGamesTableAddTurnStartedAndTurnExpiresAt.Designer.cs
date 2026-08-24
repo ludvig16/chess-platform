5bb2a66186b51d7207940a83581a9832b852a177
lusj3822 <ludvigsjoberg160@gmail.com>
2026-08-24 23:33:19 +0200

    feat: add TurnStartedAt and TurnExpiresAt attributes to Games table

--- src/ChessPlatform.Api/Infrastructure/Persistence/Migrations/20260824213024_AlterGamesTableAddTurnStartedAndTurnExpiresAt.Designer.cs
+++ src/ChessPlatform.Api/Infrastructure/Persistence/Migrations/20260824213024_AlterGamesTableAddTurnStartedAndTurnExpiresAt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ChessPlatform.Api.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ChessPlatform.Api.Migrations
 {
     [DbContext(typeof(ChessDbContext))]
-    partial class ChessDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260824213024_AlterGamesTableAddTurnStartedAndTurnExpiresAt")]
+    partial class AlterGamesTableAddTurnStartedAndTurnExpiresAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ChessPlatform.Api/Infrastructure/Persistence/Migrations/20260824213024_AlterGamesTableAddTurnStartedAndTurnExpiresAt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChessPlatform.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterGamesTableAddTurnStartedAndTurnExpiresAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TurnExpiresAt",
+                table: "Games",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TurnStartedAt",
+                table: "Games",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TurnExpiresAt",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "TurnStartedAt",
+                table: "Games");
+        }
+    }
+}
